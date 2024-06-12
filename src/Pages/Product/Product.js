6c39aa8442945c0ed353fb4287f6c48dfea9942e
Malikoxs <malika.bolchurova@gmail.com>
2024-06-12 11:17:55 +0300

    change product.css

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -3,6 +3,8 @@ import { useMatch } from "react-router-dom"
 import { AppContext } from "../../App";
 import NotFound from "../NotFound/NotFound";
 import { AddToCart } from "../../Components/AddToCart/AddToCart";
+import './Product.css';
+
 
 export default function Product() {
   const { params } = useMatch("/products/:slug");
@@ -20,7 +22,7 @@ export default function Product() {
         <img src={product.picture} alt={product.name} />
       </div>
       <div className="Product_txt">
-        <h1>{product.name}</h1>
+        <h1 className="name">{product.name}</h1>
         <p><strong>Price:</strong> {product.price}$</p>
         <p><strong>Description: </strong>{product.description}</p>
         <AddToCart product={product} />
